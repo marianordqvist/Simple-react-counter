77cb5fdfcf33d7a04b07ecb0ca253cb3673d1be2
marianordqvist <marianordqvist@live.se>
2023-12-28 16:15:31 +0100

    add clear button

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -3,7 +3,14 @@ import React, { useState } from "react";
 function Counter() {
   const [count, setCount] = useState(0);
   const handleIncrement = () => setCount(count + 1);
-  const handleDecrement = () => setCount(count - 1);
+  const handleDecrement = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
+  const handleClear = () => setCount(0);
+
   return (
     <div>
       <div className="CounterContainer">
@@ -11,6 +18,9 @@ function Counter() {
         <div className="ButtonContainer">
           <button onClick={handleIncrement} className="ButtonIncrease">
             +
+          </button>
+          <button onClick={handleClear} className="ButtonClear">
+            clear
           </button>
           <button onClick={handleDecrement} className="ButtonDecrease">
             -
